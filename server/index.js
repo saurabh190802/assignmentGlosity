34f1eb03f6eac50f13de5c8b4f738a3b0d3ae9af
saurabh190802 <saurabhsatapathy0@gmail.com>
2022-05-08 13:28:14 +0530

    pushed

--- server/index.js
+++ server/index.js
@@ -2,23 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv';
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://saurabh:saurabh123@events.tmiuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
